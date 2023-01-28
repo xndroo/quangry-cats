4aa316db8ae2f8b46de0e6d26dbc133b5679ab7f
xndroo <stephens.androo@gmail.com>
2023-01-28 15:16:29 -0800

    added metal and glass and cleaned up

---
+++ Quangry-Cats-Project/Assets/Scripts/glassBlockDestruction.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glassBlockDestruction : MonoBehaviour
+{
+    private float collisionVelocity = 0.0f;
+    private Rigidbody2D rb;
+    public float damageThreshold = 5.0f;
+    private float health;
+    public float maxHealth = 5.0f;
+    private int damageLevelblock = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	    health = maxHealth;
+    }
+
+    // called when the cube hits the floor
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        collisionVelocity = col.relativeVelocity.magnitude;
+        
+        if (collisionVelocity > damageThreshold)
+        {
+            health = health - collisionVelocity;
+        }
+    }
+
+    void Shard()
+    {
+    	var exp = GetComponent<ParticleSystem>();
+    	exp.Play();
+    	Destroy(this.gameObject, exp.duration);
+    }
+
+    void Update()
+    {
+    	if((health < 0.0f)  && (damageLevelblock < 1))
+        {
+            Shard();
+            damageLevelblock = 1;
+        }
+    }
+}
---
+++ Quangry-Cats-Project/Assets/Scripts/woodBlockDestruction.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woodBlockDestruction : MonoBehaviour
+{
+    private float collisionVelocity = 0.0f;
+    private Rigidbody2D rb;
+    public float damageThreshold = 5.0f;
+    private float health;
+    public float maxHealth = 10.0f;
+    public SpriteRenderer spriteRenderer;
+    public Sprite damaged;
+    private int damageLevelblock = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	    health = maxHealth;
+    }
+
+    // called when the cube hits the floor
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        collisionVelocity = col.relativeVelocity.magnitude;
+        
+        if (collisionVelocity > damageThreshold)
+        {
+            health = health - collisionVelocity;
+        }
+    }
+
+    void Splinter()
+    {
+    	var exp = GetComponent<ParticleSystem>();
+    	exp.Play();
+    	Destroy(this.gameObject, exp.duration);
+    }
+
+    void Update()
+    {
+    	if((health < maxHealth*0.5) && (damageLevelblock < 1))
+        {
+    	    spriteRenderer.sprite = damaged;
+            damageLevelblock = 1;
+        }
+    	if((health < 0.0f)  && (damageLevelblock < 2))
+        {
+            Splinter();
+            damageLevelblock = 2;
+        }
+    }
+}
