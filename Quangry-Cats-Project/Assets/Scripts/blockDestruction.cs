85c823f5026a37533ad88775ffff704924b3e9f7
xndroo <stephens.androo@gmail.com>
2023-01-27 20:48:31 -0800

    merge fix

--- Quangry-Cats-Project/Assets/Scripts/blockDestruction.cs
+++ Quangry-Cats-Project/Assets/Scripts/blockDestruction.cs
@@ -7,18 +7,10 @@ public class blockDestruction : MonoBehaviour
     private float collisionVelocity = 0.0f;
     private Rigidbody2D rb;
     public float damageThreshold = 5.0f;
-<<<<<<< HEAD
-<<<<<<< HEAD
     private float health;
     public float maxHealth = 10.0f;
     public SpriteRenderer spriteRenderer;
     public Sprite damaged;
-=======
-    // private float health = 3.0f;
->>>>>>> f7d776f755d049b8261157f6353c0f3db738997d
-=======
-    private float health = 3.0f;
->>>>>>> b9a09d2e26b9e673c31c12230524d180c3713b70
 
     void Start()
     {
