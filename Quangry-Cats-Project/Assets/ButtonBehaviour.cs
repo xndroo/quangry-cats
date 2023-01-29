fc625f670f2892043607200e0a4dd93b69e83314
Alezander9 <Alexander.j.yue@gmail.com>
2023-01-28 17:21:46 -0800

    menu button

--- Quangry-Cats-Project/Assets/ButtonBehaviour.cs
+++ Quangry-Cats-Project/Assets/ButtonBehaviour.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class ButtonBehaviour : MonoBehaviour {
 
+    public void OnMenuButtonPress()
+    {
+      SceneLoader.Load("Main Menu");
+    }
+
     public void OnLevel1ButtonPress()
     {
       SceneLoader.Load("Andrew Scene");
